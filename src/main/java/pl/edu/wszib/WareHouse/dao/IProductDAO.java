affc4def565b24bc72d76ad423d8a97889ed978c
K0STUR <rafalkosteck@gmail.com>
2021-03-24 21:13:58 +0100

    .

---
+++ src/main/java/pl/edu/wszib/WareHouse/dao/IProductDAO.java
@@ -0,0 +1,14 @@
+package pl.edu.wszib.warehouse.dao;
+
+import pl.edu.wszib.warehouse.model.Product;
+
+import java.util.List;
+
+public interface IProductDAO {
+    List<Product> getAllProducts();
+    void updateProduct(Product product);
+    void deleteProduct(Product product);
+    Product getProductById(int id);
+    Product getProductByCode(String code);
+
+}
